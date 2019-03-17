bf3f4c58917b073504e2989c0663ca73c6b2db8a
Emilia Ziemichód <emifrompoland@gmail.com>
2019-03-17 00:42:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,9 @@ def reverse_each_word(sentence1)
   sentence1
 end
 
-def reverse_word
+def reverse_word(sentence1)
+  sentence.each do |word|
+    word.reverse
   "Hello there, and how are you?".reverse
 end
 binding.pry
